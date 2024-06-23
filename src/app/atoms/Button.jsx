27632a11f83995008a0ec8ab2ff933eeda5c9071
MAGNET-CODE <sozan1382dastan@gmail.com>
2024-06-23 14:41:32 +0330

    add-button-component

---
+++ src/app/atoms/Button.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Button = ({ text, icon, color, py, px, borderRadius }) => {
+    return (
+        <button
+            className={`bg-${color} text-white py-${py} px-${px} rounded-${borderRadius} flex items-center justify-center`}
+        >
+            {icon && <span className="mr-2">{icon}</span>}
+            {text}
+        </button>
+    );
+};
+
+export default Button;
