acb3476e4a29bb05a61ddb09828dc344a9845356
MAGNET-CODE <sozan1382dastan@gmail.com>
2024-07-07 22:44:14 +0330

    add-section description-online-code

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,16 @@
 import Marquee from "@/components/organisms/Marquee";
+import CodeExample from "@/components/templates/CodeExample";
+import DescriptionOnlineCode from "@/components/templates/DescriptionOnlineCode";
 import HeroMain from "@/components/templates/HeroMain";
 
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col gap-y-20 items-center justify-start p-4">
+    <main className="flex min-h-screen flex-col gap-y-20 items-center justify-start">
       <HeroMain />
       <Marquee />
+      <CodeExample />
+      <DescriptionOnlineCode />
     </main>
   );
 }
---
+++ src/components/templates/DescriptionOnlineCode.jsx
@@ -0,0 +1,25 @@
+import ArrowLeftBlack from "../../../public/ArrowLeftBlack.svg"
+import TitleDescription from '../atoms/TitleDescription'
+import Button from '../atoms/Button'
+
+function DescriptionOnlineCode() {
+  return (
+    <div className="w-full bg-bg-main h-[597px] lg:h-[768px] flex items-center justify-center flex-col gap-y-4 px-4">
+        <TitleDescription 
+            title="میتونی به صورت آنلاین با سلام کار کنی 🚀"
+            description="اولین زبان برنامه نویسی فارسی، اونم کاملا رایگان باورت میشه ؟"
+        />
+        <Button
+            text="بزن بریم"
+            variant="buttonWhite"
+            type="link"
+            href="/404"
+            icon={<span>
+                <ArrowLeftBlack />
+            </span>}
+        />
+    </div>
+  )
+}
+
+export default DescriptionOnlineCode
