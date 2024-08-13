f3f80198d89e50538eca811f3dd34de171d18995
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-08-13 10:32:12 +0330

    feat : add cemment centent slider component

---
+++ src/components/home/comment-box/CommentContent.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+export default function CommentContent({ comment }) {
+  return (
+    <>
+      {/* <div className="w-full h-full px-8 py-7 flex flex-col items-center justify-between">
+              <h1 className="text-xl text-start w-full font-Estedad-SemiBold">
+                نظرم مثبته
+              </h1>
+              <p className="text-sm text-start w-full font-Estedad-Medium !leading-10">
+                در سریع تری میتوانی زبانو رو یاد بگیرید و یادگیریش خیلی ساده تری
+                ولی در هم حین تمام توانایی ای که در زبان های دیگر دارید رو اینجا
+                هم دارید .{" "}
+              </p>
+              <div className="w-full flex justify-center items-center gap-2">
+                <div className="w-auto h-full flex flex-col justify-center gap-1 items-center">
+                  <h1 className="text-base font-Estedad-SemiBold">
+                    نظرم مثبته
+                  </h1>
+                  <p className="text-[12px] font-Estedad-Regular">
+                    مدت زمان یادگیری : روز
+                  </p>
+                </div>
+                <div className="w-auto h-full flex justify-center items-center"></div>
+                <Image src={Avatar1} alt="" />
+              </div>
+            </div> */}
+      <div className="w-full h-full flex justify-center items-center">
+        CommentContent
+      </div>
+    </>
+  );
+}
