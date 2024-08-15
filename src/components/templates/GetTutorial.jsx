57538ae28c8052ed12bd0eddc8ed525e068dbe3e
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-08-15 17:07:05 +0330

    feat : add section get tutorial (no complated)

---
+++ src/components/templates/GetTutorial.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function GetTutorial() {
+  return (
+    <div className="relative w-full h-[600px] bg-bg-section mb-96 flex justify-center items-center bg-vector bg-center bg-cover bg-clip-content bg-origin-content bg-no-repeat"></div>
+  );
+}
