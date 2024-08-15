85eae74b70fff7d26ab7c65fee82aac8f64b0f30
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-08-15 17:06:43 +0330

    clean : clean code

--- src/app/Download/page.js
+++ src/app/Download/page.js
@@ -1,24 +1,22 @@
-import Button from "@/components/atoms/Button"
-import Gif from "@/components/atoms/Gif"
-import CarectersDownload from "@/components/molecules/CarectersDownload"
+import Button from "@/components/atoms/Button";
+import Gif from "@/components/atoms/Gif";
+import CarectersDownload from "@/components/molecules/CarectersDownload";
 
-import DownloadClud from "../../../public/download.svg"
-import TitleDescription from "@/components/atoms/TitleDescription"
+import DownloadClud from "../../../public/download.svg";
+import TitleDescription from "@/components/atoms/TitleDescription";
 
 function Download() {
   return (
     <div className="flex flex-col items-center justify-center gap-5">
-
-        <div className='relative w-full sm:w-fit flex items-center justify-center'>
-        <Gif variant="GifDownload">
-        </Gif>
+      <div className="relative w-full sm:w-fit flex items-center justify-center">
+        <Gif variant="GifDownload"></Gif>
         <CarectersDownload />
-        </div>
-        <TitleDescription
-          title="مرسی که ..."
-          description="نرم افزار مارو دانلود کردید و تشکر میکنیم از این کارتون !"
-        />
-        <div className="flex items-center justify-center gap-6">
+      </div>
+      <TitleDescription
+        title="مرسی که ..."
+        description="نرم افزار مارو دانلود کردید و تشکر میکنیم از این کارتون !"
+      />
+      <div className="flex items-center justify-center gap-6">
         <Button
           text="مشاهده ی آموزش"
           variant="buttonOrange2"
@@ -28,15 +26,17 @@ function Download() {
         <Button
           text="مشکل تو دانلود"
           variant="buttonOrange"
-          icon={<span>
-            <DownloadClud />
-          </span>}
+          icon={
+            <span>
+              <DownloadClud />
+            </span>
+          }
           type="link"
           href="/"
         />
-        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Download
+export default Download;
--- src/components/templates/DescriptionOnlineCode.jsx
+++ src/components/templates/DescriptionOnlineCode.jsx
@@ -1,44 +1,50 @@
-import ArrowLeftBlack from "../../../public/ArrowLeftBlack.svg"
-import TitleDescription from '../atoms/TitleDescription'
-import Vactor1 from "../../../public/Vector 12.svg"
-import Group1 from "../../../public/Group 97.svg"
-import Vactor2 from "../../../public/Group 91 2.svg"
-import Button from '../atoms/Button'
+import ArrowLeftBlack from "../../../public/ArrowLeftBlack.svg";
+import TitleDescription from "../atoms/TitleDescription";
+import Vactor1 from "../../../public/Vector 12.svg";
+import Group1 from "../../../public/Group 97.svg";
+import Vactor2 from "../../../public/Group 91 2.svg";
+import Button from "../atoms/Button";
 
 function DescriptionOnlineCode() {
   return (
     <div className="w-full bg-bg-section relative">
-        <div className={"container w-full mx-auto flex items-center justify-center flex-col gap-y-4 py-32"}>
-            <div className={"hidden md:block absolute w-11/12 2xl:w-10/12 z-10"}>
-                <Vactor1/>
-            </div>
-            <div className={"absolute w-full h-full top-1/2 -translate-y-1/2 z-10"}>
-                <Group1/>
-                <div className={"absolute w-[70px] top-20 left-1/4"}>
-                    <Vactor2/>
-                </div>
-                <div className={"absolute w-[70px] bottom-20 right-1/4"}>
-                    <Vactor2/>
-                </div>
-            </div>
-            <div className={"relative z-20"}>
-                <TitleDescription
-                    title="میتونی به صورت آنلاین با سلام کار کنی 🚀"
-                    description="اولین زبان برنامه نویسی فارسی، اونم کاملا رایگان باورت میشه ؟"
-                />
-            </div>
-            <Button
-                text="بزن بریم"
-                variant="buttonWhite"
-                type="link"
-                href="/404"
-                icon={<span>
-                <ArrowLeftBlack />
-                </span>}
-            />
+      <div
+        className={
+          "container w-full mx-auto flex items-center justify-center flex-col gap-y-4 py-32"
+        }
+      >
+        <div className={"hidden md:block absolute w-11/12 2xl:w-10/12 z-10"}>
+          <Vactor1 />
         </div>
+        <div className={"absolute w-full h-full top-1/2 -translate-y-1/2 z-10"}>
+          <Group1 />
+          <div className={"absolute w-[70px] top-20 left-1/4"}>
+            <Vactor2 />
+          </div>
+          <div className={"absolute w-[70px] bottom-20 right-1/4"}>
+            <Vactor2 />
+          </div>
+        </div>
+        <div className={"relative z-20"}>
+          <TitleDescription
+            title="میتونی به صورت آنلاین با سلام کار کنی 🚀"
+            description="اولین زبان برنامه نویسی فارسی، اونم کاملا رایگان باورت میشه ؟"
+          />
+        </div>
+        <Button
+          text="بزن بریم"
+          variant="buttonWhite"
+          type="link"
+          href="/404"
+          icon={
+            <span>
+              <ArrowLeftBlack />
+            </span>
+          }
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default DescriptionOnlineCode
+export default DescriptionOnlineCode;
