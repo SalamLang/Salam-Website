02eece09ae8d29816b05b12e09de3f9977523337
mohamadreza <mohamadreza1388.org@gmail.com>
2024-08-17 20:31:36 +0330

    feat(swipper): add swipper to marquee section

--- src/components/organisms/Marquee.jsx
+++ src/components/organisms/Marquee.jsx
@@ -1,55 +1,20 @@
-import Snap from "../../../public/Snap.png";
-import Digikala from "../../../public/Digikala.png";
-import Asrez from "../../../public/Asrez.png";
-import Tapsi from "../../../public/Tapsi.png";
-import TapsiGarage from "../../../public/TapsiGarage.png";
-import Image from "next/image";
+"use client";
+
+import {Swiper, SwiperSlide} from "swiper/react";
+import "swiper/css";
+import React from "react";
 
 
 function Marquee() {
-  return (
-    <div className="w-full overflow-hidden">
-      <marquee className="flex animate-marquee items-center gap-12">
-        <ul className="flex items-center justify-center gap-12">
-          <li className="">
-            <Image
-              src={Snap}
-              alt="Snap"
-              className=""
-            />
-          </li>
-          <li className="">
-            <Image
-              src={Digikala}
-              alt="Digikala"
-              className=""
-            />
-          </li>
-          <li className="">
-            <Image
-              src={Asrez}
-              alt="Asrez"
-              className=""
-            />
-          </li>
-          <li className="">
-            <Image
-              src={Tapsi}
-              alt="Tapsi"
-              className=""
-            />
-          </li>
-          <li className="">
-            <Image
-              src={TapsiGarage}
-              alt="Tapsi Garage"
-              className=""
-            />
-          </li>
-        </ul>
-      </marquee>
-    </div>
-  );
+    return (
+        <div className={"w-full overflow-hidden"}>
+            <Swiper>
+                <SwiperSlide>
+
+                </SwiperSlide>
+            </Swiper>
+        </div>
+    )
 }
 
 export default Marquee;
