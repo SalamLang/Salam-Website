e3ee7cbde99621bd6d198ee7b376deb8e058b529
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-08-24 17:20:19 +0330

    (feat) : add class pros in avatar component

--- src/components/atoms/Avatar.jsx
+++ src/components/atoms/Avatar.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
 
-const Avatar = ({ src, alt, width, height }) => {
-    return (
-        <div className="w-16 h-16 md:w-24 md:h-24 rounded-full overflow-hidden">
-            <Image src={src} alt={alt} width={width} height={height} className="object-cover" />
-        </div>
-    );
+const Avatar = ({ src, alt, width, height, className }) => {
+  return (
+    <div className="w-16 h-16 md:w-24 md:h-24 rounded-full overflow-hidden">
+      <Image
+        src={src}
+        alt={alt}
+        width={width}
+        height={height}
+        className={`object-cover ${className || ""}`}
+      />
+    </div>
+  );
 };
 
 export default Avatar;
