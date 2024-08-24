cab9564eae58018b4a4a88bab1353294d7cae7cb
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-08-24 17:21:34 +0330

    (feat) change weigh font

--- src/components/atoms/TitleDescription.jsx
+++ src/components/atoms/TitleDescription.jsx
@@ -1,13 +1,16 @@
-
-import React from 'react';
+import React from "react";
 
 const TitleDescription = ({ title, description }) => {
-    return (
-        <div className="flex items-center justify-center flex-col gap-6 z-50">
-            <h1 className="text-xl md:text-2xl lg:text-3xl font-Estedad-Medium text-title text-center">{title}</h1>
-            <p className="text-description text-center font-Estedad-Light text-gray-500 text-xs lg:text-sm">{description}</p>
-        </div>
-    );
+  return (
+    <div className="flex items-center justify-center flex-col gap-6 z-50">
+      <h1 className="text-xl md:text-2xl lg:text-3xl font-Estedad-SemiBold text-title text-center">
+        {title}
+      </h1>
+      <p className="text-description text-center font-Estedad-Medium text-gray-500 text-xs">
+        {description}
+      </p>
+    </div>
+  );
 };
 
 export default TitleDescription;
