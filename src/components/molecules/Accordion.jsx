719cb16e82e858c6129c02d14df81f3223ef069d
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-08-24 17:22:52 +0330

    (refactore) accordian component has bees refactored

--- src/components/molecules/Accordion.jsx
+++ src/components/molecules/Accordion.jsx
@@ -1,55 +1,47 @@
-'use client';
-
 import React from "react";
+import { motion } from "framer-motion";
 import Image from "next/image";
 
-const Accordion = ({ title, content, index, number, active, handleToggle }) => {
+const Accordion = ({ number, title, content, index, active, handleToggle }) => {
   return (
-    <div className='m-0 p-0 w-full max-w-[929px]'>
-      <div className='flex flex-col'>
-        <div className='flex justify-between items-center gap-3 my-2 cursor-pointer'>
-          <div className="flex items-center justify-start gap-6">
-            <span className="bg-bg-main w-[60px] h-[60px] rounded-2xl text-white text-3xl flex items-center justify-center">
-                {number}
-            </span>
-            <h4
-                className="text-title text-2xl"
-                onClick={handleToggle}>
-                {title}
-            </h4>
-          </div>
-
+    <div className="w-full max-w-full">
+      <div
+        className="cursor-pointer p-4 flex justify-between items-center"
+        onClick={handleToggle}
+      >
+        <h3 className="text-2xl font-Estedad-Medium flex justify-start items-center gap-5">
+          <span className="bg-bg-main w-[60px] h-[60px] rounded-2xl text-white text-3xl flex items-center justify-center">
+            {number}
+          </span>
+          {title}
+        </h3>
+        <motion.span
+          className="w-auto h-auto"
+          initial={{ rotate: 0 }}
+          animate={{ rotate: active ? -90 : 0 }}
+        >
           <Image
             src="/ArrowLeftFaq.svg"
             width={10}
             height={6}
-            alt='arrow-icon'
+            alt="arrow-icon"
             onClick={handleToggle}
-            className={
-              active
-                ? "cursor-pointer -rotate-90"
-                : "cursor-pointer"
-            }
+            className={"cursor-pointer"}
           />
-        </div>
-        <hr
-          className={
-            active
-              ? "border-border-grey2 border-b-2 relative top-[50px] opacity-20"
-              : "border-border-grey2 border-b-2 relative top-[8px]"
-          }
-        />
+        </motion.span>
       </div>
 
-      <p
-        onClick={handleToggle}
-        className={
-          active
-            ? "text-description text-[17px] w-5/6 text-start p-0 m-0 cursor-pointer pr-[85px]"
-            : "invisible max-h-0"
-        }>
-        {content}
-      </p>
+      <motion.div
+        initial={{ height: 0 }}
+        animate={{ height: active ? "auto" : 0 }}
+        transition={{ duration: 0.3 }}
+        className="overflow-hidden w-full"
+      >
+        <div className="p-4">
+          <p>{content}</p>
+        </div>
+      </motion.div>
+      <hr className={"border-border-grey2 border-b-2 relative opacity-60"} />
     </div>
   );
 };
