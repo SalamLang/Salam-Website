16940f05bdd96aa5b92b2bdd12781f171b526695
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-08-24 17:20:52 +0330

    (fix) change manual width to auto

--- src/components/atoms/ButtonCodeBox.jsx
+++ src/components/atoms/ButtonCodeBox.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function ButtonCodeBox({ icon }) {
   return (
-    <button className="w-14 h-14 md:w-16 md:h-16 bg-bg-button-orange rounded-xl flex items-center justify-center cursor-pointer">
-        {icon}
+    <button className="w-auto p-2 md:p-4 bg-bg-button-orange rounded-xl flex items-center justify-center cursor-pointer">
+      {icon}
     </button>
-  )
+  );
 }
 
-export default ButtonCodeBox
+export default ButtonCodeBox;
