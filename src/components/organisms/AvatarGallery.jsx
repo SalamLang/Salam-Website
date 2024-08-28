5b90b03339ff938e4c657b17644019cd332cb875
ALi Josheghani <ali.josheghani2005@gmail.com>
2024-08-28 21:28:00 +0330

    (fix) remove Avatar .png becouse did not exit on public icons

--- src/components/organisms/AvatarGallery.jsx
+++ src/components/organisms/AvatarGallery.jsx
@@ -1,32 +1,32 @@
 import React from "react";
 import AvatarGroup from "../molecules/AvatarGroup";
-import Avatar1 from "/public/Avatar.png";
-import Avatar2 from "/public/Avatar2.png";
-import Avatar3 from "/public/Avatar3.png";
+// import Avatar2 from "../../../public/Avatar.png";
+import Avatar2 from "../../../public/Avatar2.png";
+import Avatar3 from "../../../public/Avatar3.png";
 
 const avatars = [
-  { src: Avatar1, alt: "User 1", width: 150, height: 150 },
+  { src: Avatar2, alt: "User 1", width: 150, height: 150 },
   { src: Avatar2, alt: "User 2", width: 150, height: 150 },
   { src: Avatar3, alt: "User 3", width: 150, height: 150 },
-  { src: Avatar1, alt: "User 1", width: 150, height: 150 },
+  { src: Avatar2, alt: "User 1", width: 150, height: 150 },
   { src: Avatar2, alt: "User 2", width: 150, height: 150 },
   { src: Avatar3, alt: "User 3", width: 150, height: 150 },
-  { src: Avatar1, alt: "User 1", width: 150, height: 150 },
+  { src: Avatar2, alt: "User 1", width: 150, height: 150 },
   { src: Avatar2, alt: "User 2", width: 150, height: 150 },
   { src: Avatar3, alt: "User 3", width: 150, height: 150 },
-  { src: Avatar1, alt: "User 1", width: 150, height: 150 },
+  { src: Avatar2, alt: "User 1", width: 150, height: 150 },
   { src: Avatar2, alt: "User 2", width: 150, height: 150 },
   { src: Avatar3, alt: "User 3", width: 150, height: 150 },
-  { src: Avatar1, alt: "User 1", width: 150, height: 150 },
+  { src: Avatar2, alt: "User 1", width: 150, height: 150 },
   { src: Avatar2, alt: "User 2", width: 150, height: 150 },
   { src: Avatar3, alt: "User 3", width: 150, height: 150 },
-  { src: Avatar1, alt: "User 1", width: 150, height: 150 },
+  { src: Avatar2, alt: "User 1", width: 150, height: 150 },
   { src: Avatar2, alt: "User 2", width: 150, height: 150 },
   { src: Avatar3, alt: "User 3", width: 150, height: 150 },
-  { src: Avatar1, alt: "User 1", width: 150, height: 150 },
+  { src: Avatar2, alt: "User 1", width: 150, height: 150 },
   { src: Avatar2, alt: "User 2", width: 150, height: 150 },
   { src: Avatar3, alt: "User 3", width: 150, height: 150 },
-  { src: Avatar1, alt: "User 1", width: 150, height: 150 },
+  { src: Avatar2, alt: "User 1", width: 150, height: 150 },
   { src: Avatar2, alt: "User 2", width: 150, height: 150 },
   { src: Avatar3, alt: "User 3", width: 150, height: 150 },
 ];
