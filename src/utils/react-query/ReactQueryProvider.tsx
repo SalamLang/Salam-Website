746032715e1695d80621102d344ae549714540cd
ALi Josheghani <ali.josheghani2005@gmail.com>
2024-08-31 21:27:08 +0330

    feat: Add ReactQueryProvider component for managing React Query state

---
+++ src/utils/react-query/ReactQueryProvider.tsx
@@ -0,0 +1,10 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+
+export const queryClient = new QueryClient();
+
+export default function ReactQueryProvider({ children }) {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
