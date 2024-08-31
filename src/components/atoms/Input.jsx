eb596d03016f8c5c58329648d8226fce1be4147a
ALi Josheghani <ali.josheghani2005@gmail.com>
2024-08-31 21:27:24 +0330

    refactor: Update Input component to accept className prop

--- src/components/atoms/Input.jsx
+++ src/components/atoms/Input.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-function Input() {
+function Input({ className, ...attr }) {
   return (
     <input
-      className="text-black !text-base md:text-lg font-Estedad-Medium h-10 max-sm:w-full max-w-[150px] sm:max-w-[200px] md:max-w-[260px] placeholder:text-black bg-transparent border-none outline-none"
-      type="text"
-      placeholder="مثال: ۰۹۹۱۳۹۱۵۲۵۴"
-      maxLength={11}
+      className={`text-black !text-base md:text-lg font-Estedad-Medium h-10 max-sm:w-full max-w-[150px] sm:max-w-[200px] md:max-w-[260px] placeholder:text-black bg-transparent border-none outline-none ${
+        className || ""
+      }`}
+      {...attr}
     />
   );
 }
