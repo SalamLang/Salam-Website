69ec7de2ab7dc2269b16cde0514d2f0a24916cf7
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-09-07 15:20:16 +0330

    chore: Update TitleDescription component to accept additional props

--- src/components/atoms/TitleDescription.jsx
+++ src/components/atoms/TitleDescription.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-const TitleDescription = ({ title, description }) => {
+const TitleDescription = ({ title, description , headerClass,descClass , className }) => {
   return (
-    <div className="flex items-center justify-center flex-col gap-6 z-50">
-      <h1 className="text-xl md:text-2xl lg:text-3xl font-Estedad-SemiBold text-title text-center">
+    <div className={`flex items-center justify-center flex-col gap-6 z-50 ${className || ""}`}>
+      <h1 className={`text-xl md:text-2xl lg:text-3xl font-Estedad-SemiBold text-title text-center ${headerClass || ""}`}>
         {title}
       </h1>
-      <p className="text-description text-center font-Estedad-Medium text-gray-500 text-xs">
+      <p className={`text-description text-center font-Estedad-Medium text-gray-500 text-xs ${descClass || ""}`}>
         {description}
       </p>
     </div>
