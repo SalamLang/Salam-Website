6ba564a2b99442fab6c70aa547c407e546405e38
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-09-07 15:20:02 +0330

    chore: Update Button component styles for improved responsiveness

--- src/components/atoms/Button.jsx
+++ src/components/atoms/Button.jsx
@@ -5,25 +5,25 @@ const Button = ({ text, variant, icon, padding, borderRadius, type, href, classN
 
     switch (variant) {
         case 'buttonOrange':
-            classes += 'hover:scale-105 transition bg-bg-button-orange h-11 text-text-button-white flex items-center justify-center gap-2';
+            classes += 'hover:scale-105 transition bg-bg-button-orange h-10 text-text-button-white flex items-center justify-center gap-2';
             break;
         case 'buttonOrange2':
-            classes += 'hover:scale-105 transition bg-bg-button-orange2 h-11 text-text-button-orange2 flex items-center justify-center gap-2';
+            classes += 'hover:scale-105 transition bg-bg-button-orange2 h-10 text-text-button-orange2 flex items-center justify-center gap-2';
             break;
         case 'buttonBlue':
-            classes += 'hover:scale-105 transition bg-bg-button-blue h-11 text-text-button-white flex items-center justify-center gap-2';
+            classes += 'hover:scale-105 transition bg-bg-button-blue h-10 text-text-button-white flex items-center justify-center gap-2';
             break;
         case 'buttonBlue2':
-            classes += 'hover:scale-105 transition bg-bg-button-blue-2 h-11 text-text-button-orange2 font-Estedad-Light flex items-center justify-center gap-2';
+            classes += 'hover:scale-105 transition bg-bg-button-blue-2 h-10 text-text-button-orange2 font-Estedad-Light flex items-center justify-center gap-2';
             break;
         case 'buttonWhite':
-            classes += 'hover:scale-105 transition z-20 bg-bg-button-white h-11 text-text-button-black flex items-center justify-center gap-2';
+            classes += 'hover:scale-105 transition z-20 bg-bg-button-white h-10 text-text-button-black flex items-center justify-center gap-2';
             break;
         case 'buttonGreen':
-            classes += 'hover:scale-105 transition bg-bg-button-green h-11 text-text-button-black flex items-center justify-center gap-2';
+            classes += 'hover:scale-105 transition bg-bg-button-green h-10 text-text-button-black flex items-center justify-center gap-2';
             break;
         case 'buttonBlack':
-            classes += 'hover:scale-105 transition bg-bg-button-black h-11 text-text-button-white flex items-center justify-center gap-2';
+            classes += 'hover:scale-105 transition bg-bg-button-black h-10 text-text-button-white flex items-center justify-center gap-2';
             break;
     }
 
