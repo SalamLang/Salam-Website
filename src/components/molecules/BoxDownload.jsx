43db92e57889819afa57d81c35be22fa718d87bb
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-09-07 15:20:32 +0330

    chore: Update SVG icons for Linux, Mac, Android, Windows, and PWA components

--- src/components/molecules/BoxDownload.jsx
+++ src/components/molecules/BoxDownload.jsx
@@ -1,45 +1,45 @@
-import Image from "next/image";
+"use client";
 
-import Linux from "../../../public/linux.png";
-import Mac from "../../../public/mac.png";
-import Android from "../../../public/android.png";
-import Windows from "../../../public/windows.png";
-import Pwa from "../../../public/pwa.png";
+import Linux from "../../../public/linux.svg";
+import Mac from "../../../public/mac.svg";
+import Android from "../../../public/android.svg";
+import Windows from "../../../public/windows.svg";
+import Pwa from "../../../public/pwa.svg";
 
-function BoxDownload({ variant, className, imgClass , isBlack }) {
-  let classes = `w-full ${imgClass} ${isBlack && "!brightness-0"}`;
-  let src;
+function BoxDownload({ variant, className, imgClass, isBlack }) {
+  let classes = `scale-[0.65] ${imgClass} ${isBlack && "!brightness-0"}`;
+  let svgComponent = null;
   let title;
   let description;
 
   switch (variant) {
     case "Linux":
       classes;
-      src = Linux;
+      svgComponent = <Linux className={classes} />;
       title = "لینوکس";
       description = "12 مگابایت";
       break;
     case "Mac":
       classes;
-      src = Mac;
+      svgComponent = <Mac className={classes} />;
       title = "مک";
       description = "47 مگابایت";
       break;
     case "Android":
       classes;
-      src = Android;
+      svgComponent = <Android className={classes} />;
       title = "اندروید";
       description = "56 مگابایت";
       break;
     case "Windows":
       classes;
-      src = Windows;
+      svgComponent = <Windows className={classes} />;
       title = "ویندوز";
       description = "27 مگابایت";
       break;
     case "Pwa":
       classes;
-      src = Pwa;
+      svgComponent = <Pwa className={classes} />;
       title = "PWA";
       description = "بدون نیاز به نصب";
       break;
@@ -48,13 +48,9 @@ function BoxDownload({ variant, className, imgClass , isBlack }) {
   return (
     <div className="flex items-center justify-center flex-col gap-2 cursor-pointer">
       <div
-        className={`bg-bg-box rounded-xl p-4 flex items-center justify-center  w-16 h-16 2xl:w-20 2xl:h-20 ${className}`}
+        className={`bg-bg-box rounded-lg flex items-center justify-center  w-16 h-16 2xl:w-20 2xl:h-20 ${className}`}
       >
-        <Image
-          className={classes}
-          src={src}
-          alt={title} // افزودن alt برای دسترس‌پذیری
-        />
+        {svgComponent}
       </div>
       <h4 className="text-title text-center font-Estedad-SemiBold">{title}</h4>
       <p className="text-description text-center text-xs font-Estedad-Regular w-24">
