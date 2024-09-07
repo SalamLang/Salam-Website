8d87266cd7f8be62c32537a22e12d893720a49f4
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-09-07 15:19:57 +0330

    feat: Add tooltip functionality to Avatar component

--- src/components/atoms/Avatar.jsx
+++ src/components/atoms/Avatar.jsx
@@ -1,7 +1,11 @@
+"use client";
+
 import React from "react";
 import Image from "next/image";
+import { Tooltip } from "react-tooltip";
+import "react-tooltip/dist/react-tooltip.css";
 
-const Avatar = ({ src, alt, width, height, className }) => {
+const Avatar = ({ src, alt, width, height, className , tooltip_text }) => {
   return (
     <div className="w-16 h-16 md:w-24 md:h-24 rounded-full overflow-hidden">
       <Image
@@ -10,7 +14,11 @@ const Avatar = ({ src, alt, width, height, className }) => {
         width={width}
         height={height}
         className={`object-cover ${className || ""}`}
+        data-tooltip-id="my-tooltip"
+        data-tooltip-place="bottom"
+        data-tooltip-content={tooltip_text || "بدون تولتیپ"}
       />
+      <Tooltip id="my-tooltip" className="max-md:scale-[0.6] -my-8" />
     </div>
   );
 };
