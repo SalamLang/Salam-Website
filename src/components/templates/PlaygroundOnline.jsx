86b2113bbd8e3d7c030491e4a22dcd2242acbec4
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-09-08 14:38:14 +0330

    chore: Update PlaygroundOnline component to improve responsiveness and add welcome message

--- src/components/templates/PlaygroundOnline.jsx
+++ src/components/templates/PlaygroundOnline.jsx
@@ -5,13 +5,25 @@ import Editor from "../molecules/Editor";
 import { PlaygroundContextValue } from "@/utils/contexts/Playground";
 import ActionToType from "../molecules/ActionToType";
 import KeyPressFullScreen from "../molecules/KeyPressFullScreen";
+import { Alert } from "../common/Alert";
 
 export default function PlaygroundOnline() {
   const { isHidden } = useContext(PlaygroundContextValue);
   return (
-    <div className="relative w-full h-full overflow-hidden flex-grow flex justify-center items-start md:items-center">
-      {isHidden && <KeyPressFullScreen />}
-      {isHidden ? <ActionToType /> : <Editor />}
-    </div>
+    <>
+      {isHidden && (
+        <Alert
+          intent="orange"
+          size="medium"
+          className="min-h-[60px] md:min-h-[65px] lg:!min-h-[70px] flex justify-center items-center"
+        >
+          به دنیای سلام خوش اومدی اینجا دیگه هیچ محدودیتی نداری
+        </Alert>
+      )}
+      <div className="relative w-full h-full overflow-hidden flex-grow flex justify-center items-start md:items-center">
+        {isHidden && <KeyPressFullScreen />}
+        {isHidden ? <ActionToType /> : <Editor />}
+      </div>
+    </>
   );
 }
