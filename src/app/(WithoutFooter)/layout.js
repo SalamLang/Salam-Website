286fec9cc248e6fdb6d89efbb1e23562e818f714
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-09-08 14:38:18 +0330

    chore: Remove unnecessary Alert component from RootLayout

--- src/app/(WithoutFooter)/layout.js
+++ src/app/(WithoutFooter)/layout.js
@@ -4,7 +4,6 @@ import "../globals.css";
 import "../../styles/font.css";
 import SecondHeader from "@/components/templates/SecondHeader";
 import { Toaster } from "react-hot-toast";
-import { Alert } from "@/components/common/Alert";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -33,14 +32,6 @@ export default function RootLayout({ children }) {
 
         <div className="relative w-full min-h-screen overflow-auto flex flex-col justify-start">
           <SecondHeader />
-          <Alert
-            intent="orange"
-            size="medium"
-            className="min-h-[60px] md:min-h-[65px] lg:!min-h-[70px] flex justify-center items-center"
-          >
-            به دنیای سلام خوش اومدی اینجا دیگه هیچ محدودیتی نداری
-          </Alert>
-
           <div className="w-full h-full flex flex-col flex-grow">
             {children}
           </div>
