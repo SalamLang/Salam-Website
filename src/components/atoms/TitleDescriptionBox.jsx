f3a1855b19a1c1d78410d1e4578883ef07f24677
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-09-08 12:26:19 +0330

    chore: Refactor TitleDescriptionBox component to improve code readability and add flexibility

--- src/components/atoms/TitleDescriptionBox.jsx
+++ src/components/atoms/TitleDescriptionBox.jsx
@@ -1,13 +1,32 @@
+import React from "react";
 
-import React from 'react';
-
-const TitleDescriptionBox = ({ title, description }) => {
-    return (
-        <div className="flex items-center justify-center flex-col gap-3">
-            <h1 className="text-title text-base md:text-2xl font-Estedad-Light text-center">{title}</h1>
-            <p className="text-description text-center text-sm md:text-base font-Estedad-ExtraLight leading-[45px]">{description}</p>
-        </div>
-    );
+const TitleDescriptionBox = ({
+  title,
+  description,
+  className,
+  headerClass,
+  descClass,
+}) => {
+  return (
+    <div
+      className={`flex items-center justify-center flex-col gap-3 ${
+        className || ""
+      }`}
+    >
+      <h1
+        className={`text-title text-base md:text-2xl font-Estedad-Light text-center ${
+          headerClass || ""
+        }`}
+      >
+        {title}
+      </h1>
+      <p
+        className={`text-description text-center text-sm md:text-base font-Estedad-ExtraLight leading-[45px] ${descClass}`}
+      >
+        {description}
+      </p>
+    </div>
+  );
 };
 
 export default TitleDescriptionBox;
