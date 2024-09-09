e186a0039a9988353a350029e938756ccd1f8be0
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-09-09 17:10:33 +0330

    Refactor: Update GoToLearn component to add link to playground

--- src/components/templates/GoToLearn.jsx
+++ src/components/templates/GoToLearn.jsx
@@ -1,29 +1,31 @@
-import React from 'react'
-import TitleDescription from '../atoms/TitleDescription'
-import Button from '../atoms/Button'
+import React from "react";
+import TitleDescription from "../atoms/TitleDescription";
+import Button from "../atoms/Button";
 
-import ArrowLeft from "../../../public/arrowleft.svg"
-import CarecterGoToLearn from '../molecules/CarecterGoToLearn'
+import ArrowLeft from "../../../public/arrowleft.svg";
+import CarecterGoToLearn from "../molecules/CarecterGoToLearn";
 
 function GoToLearn() {
   return (
     <div className="flex items-center justify-center flex-col gap-6 relative w-full mb-96">
-        <TitleDescription
-            title="به جمع دوس داشتنی سلام ما بپیوند"
-            description="اولین زبان برنامه نویسی فارسی، اونم کاملا رایگان باورت میشه ؟"
-        />
-        <Button
-            text="بزن بریم"
-            variant="buttonBlue"
-            type="link"
-            href="/404"
-            icon={<span>
-                <ArrowLeft />
-            </span>}
-        />
-        <CarecterGoToLearn />
+      <TitleDescription
+        title="به جمع دوس داشتنی سلام ما بپیوند"
+        description="اولین زبان برنامه نویسی فارسی، اونم کاملا رایگان باورت میشه ؟"
+      />
+      <Button
+        text="بزن بریم"
+        variant="buttonBlue"
+        type="link"
+        href="/playground"
+        icon={
+          <span>
+            <ArrowLeft />
+          </span>
+        }
+      />
+      <CarecterGoToLearn />
     </div>
-  )
+  );
 }
 
-export default GoToLearn
+export default GoToLearn;
