623832ed4412f53cbe5b851aeddb1002e338bb0f
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-09-09 17:10:17 +0330

    Refactor: Update EducationDownload component for improved styling and link destination

--- src/components/templates/EducationDownload.jsx
+++ src/components/templates/EducationDownload.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import TitleDescription from "../atoms/TitleDescription";
 import BoxDownload from "../molecules/BoxDownload";
 import { Button } from "../common/Button";
+import { getOS } from "@/utils/helper/getOs";
 
 function EducationDownload() {
+  const clinetOS = getOS();
   return (
     <div className="flex items-center justify-center flex-col gap-5">
       <TitleDescription title="اولین قدم اینه که میری برای دانلود نرم افزار" />
@@ -13,16 +15,37 @@ function EducationDownload() {
           rounded="full"
           size="medium"
           className="!bg-bg-main !px-10 !py-1"
+          link={`download?os=${clinetOS}`} // This is a link to the download page
         >
           دانلود
         </Button>
       </div>
       <div className="w-full flex items-center justify-evenly flex-wrap gap-8">
-        <BoxDownload variant="Linux" imgClass="scale-75" />
-        <BoxDownload variant="Windows" imgClass="scale-75" />
-        <BoxDownload variant="Mac" imgClass="scale-75" />
-        <BoxDownload variant="Android" imgClass="scale-75" />
-        <BoxDownload variant="Pwa" imgClass="scale-75" />
+        <BoxDownload
+          variant="Linux"
+          imgClass="scale-75"
+          href={`download?os=Linux`}
+        />
+        <BoxDownload
+          variant="Windows"
+          imgClass="scale-75"
+          href={`download?os=Windows`}
+        />
+        <BoxDownload
+          variant="Mac"
+          imgClass="scale-75"
+          href={`download?os=Mac`}
+        />
+        <BoxDownload
+          variant="Android"
+          imgClass="scale-75"
+          href={`download?os=Android`}
+        />
+        <BoxDownload
+          variant="Pwa"
+          imgClass="scale-75"
+          href={`download?os=Pwa`}
+        />
       </div>
     </div>
   );
