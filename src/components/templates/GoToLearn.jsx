f27c94057ae86e5b739a301e34e2624cfb53f440
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-09-10 16:15:53 +0330

    Refactor: Update GoToLearn component layout and styles

--- src/components/templates/GoToLearn.jsx
+++ src/components/templates/GoToLearn.jsx
@@ -7,7 +7,7 @@ import CarecterGoToLearn from "../molecules/CarecterGoToLearn";
 
 function GoToLearn() {
   return (
-    <div className="flex items-center justify-center flex-col gap-6 relative w-full mb-96">
+    <div className="flex items-center justify-center flex-col gap-6 relative w-full mb-20">
       <TitleDescription
         title="به جمع دوس داشتنی سلام ما بپیوند"
         description="اولین زبان برنامه نویسی فارسی، اونم کاملا رایگان باورت میشه ؟"
