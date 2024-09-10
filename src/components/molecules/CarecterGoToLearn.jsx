46def8863a0bbc72fc2101c980a9be6cb96ee01a
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-09-10 16:14:47 +0330

    Refactor: Update CarecterGoToLearn component layout and styles

--- src/components/molecules/CarecterGoToLearn.jsx
+++ src/components/molecules/CarecterGoToLearn.jsx
@@ -1,26 +1,25 @@
-import CarecterBlue from "../../../public/caracter2.svg"
-import CarecterOrange from "../../../public/caracter4.svg"
-import CaracterGreen from "../../../public/carecter7.svg"
-import CaracterGreentow from "../../../public/carecter8.svg"
-
+import CarecterBlue from "../../../public/caracter2.svg";
+import CarecterOrange from "../../../public/caracter4.svg";
+import CaracterGreen from "../../../public/carecter7.svg";
+import CaracterGreentow from "../../../public/carecter8.svg";
 
 function CarecterGoToLearn() {
   return (
     <>
-    <span className="absolute right-[65%] top-10">
+      <span className="absolute left-[10%] -z-10 md:right-[65%] top-36 md:top-10">
         <CarecterBlue />
-    </span>
-    <span className="absolute right-[55%] top-72">
+      </span>
+      <span className="absolute max-md:hidden -z-10 md:right-[55%] top-72">
         <CarecterOrange />
-    </span>
-    <span className="absolute right-[40%] top-64">
+      </span>
+      <span className="absolute max-md:hidden -z-10 md:right-[40%] top-64">
         <CaracterGreentow />
-    </span>
-    <span className="absolute right-[18%] top-6">
+      </span>
+      <span className="absolute right-[10%] -z-10 md:right-[18%] top-[12rem] md:top-7">
         <CaracterGreen />
-    </span>
+      </span>
     </>
-  )
+  );
 }
 
-export default CarecterGoToLearn
+export default CarecterGoToLearn;
