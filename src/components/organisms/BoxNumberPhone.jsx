90cb8c54ab0473350ab4ffc43a421abf36db9be2
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-09-10 16:14:52 +0330

    Refactor: Update BoxNumberPhone component layout and styles

--- src/components/organisms/BoxNumberPhone.jsx
+++ src/components/organisms/BoxNumberPhone.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import TitleDescription from '../atoms/TitleDescription'
-import BoxSentCode from '../molecules/BoxSentCode'
+import React from "react";
+import TitleDescription from "../atoms/TitleDescription";
+import BoxSentCode from "../molecules/BoxSentCode";
 
 function BoxNumberPhone() {
   return (
     <div className="w-full max-w-[1093px] rounded-3xl py-24 px-10 flex items-center justify-center flex-col gap-y-6">
-        <TitleDescription
-            title="شمارت  رو وارد از آپدیت های ما با خبرشی !"
-            description="اینجوری میتونی در سریع ترین زمان ممکن از آپدیت ها ما با خبر شی."
-        />
+      <TitleDescription
+        title="شمارت  رو وارد از آپدیت های ما با خبرشی !"
+        description="اینجوری میتونی در سریع ترین زمان ممکن از آپدیت ها ما با خبر شی."
+      />
+      <div className="w-full h-full flex flex-col justify-center items-center max-sm:gap-1 max-md:gap-2 gap-3">
         <BoxSentCode />
+      </div>
     </div>
-  )
+  );
 }
 
-export default BoxNumberPhone
+export default BoxNumberPhone;
