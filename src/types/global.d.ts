e31b2fe7890995af43092cdefa060e162c9717f9
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-09-12 17:22:02 +0330

    chore: Update global.d.ts to include module declarations for image file types

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -10,3 +10,16 @@ declare module "platform" {
   };
   export default platform;
 }
+
+declare module "*.svg" {
+  const content: any;
+  export default content;
+}
+declare module "*.webp" {
+  const content: any;
+  export default content;
+}
+declare module "*.png" {
+  const content: any;
+  export default content;
+}
