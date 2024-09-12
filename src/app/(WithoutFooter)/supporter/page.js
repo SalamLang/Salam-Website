89e476633c93d71842ad56615f1e837e7fd017b0
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-09-12 17:21:44 +0330

    feat: Add new page for supporters without footer

---
+++ src/app/(WithoutFooter)/supporter/page.js
@@ -0,0 +1,10 @@
+import SupporterContent from "@/components/templates/SupporterContent";
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="w-full h-full flex-grow flex justify-center items-start md:items-center max-md:py-20">
+      <SupporterContent />
+    </div>
+  );
+}
