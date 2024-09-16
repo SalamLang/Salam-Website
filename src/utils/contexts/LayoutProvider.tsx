2818300ecccde33ccc1b258afcf43b33d315247f
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-09-16 17:00:31 +0330

    Refactor: Update LayoutProvider to add dark mode support

--- src/utils/contexts/LayoutProvider.tsx
+++ src/utils/contexts/LayoutProvider.tsx
@@ -1,11 +1,13 @@
 "use client";
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 
 export const LayoutContext = createContext({
   headerType: "default",
   setHeaderType: null,
   isFooterHidden: false,
   setIsFooterHidden: null,
+  isDark: false,
+  setIsDark: null,
 });
 
 export function LayoutProvider({ children }) {
@@ -13,10 +15,25 @@ export function LayoutProvider({ children }) {
     "default"
   );
   const [isFooterHidden, setIsFooterHidden] = useState(false);
+  const [isDark, setIsDark] = useState(() => {
+    const savedState = localStorage.getItem("isDarkHeader");
+    return savedState ? JSON.parse(savedState) : false;
+  });
+
+  useEffect(() => {
+    localStorage.setItem("isDarkHeader", JSON.stringify(isDark));
+  }, [isDark]);
 
   return (
     <LayoutContext.Provider
-      value={{ headerType, setHeaderType, isFooterHidden, setIsFooterHidden }}
+      value={{
+        headerType,
+        setHeaderType,
+        isFooterHidden,
+        setIsFooterHidden,
+        isDark,
+        setIsDark,
+      }}
     >
       {children}
     </LayoutContext.Provider>
