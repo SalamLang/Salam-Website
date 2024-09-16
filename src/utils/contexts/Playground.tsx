1f367d4e4984870f751564342526d0aaf6d1546d
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-09-16 17:00:35 +0330

    Refactor: Update PlaygroundContext to persist theme and fontName in local storage

--- src/utils/contexts/Playground.tsx
+++ src/utils/contexts/Playground.tsx
@@ -1,23 +1,34 @@
 "use client";
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 
 export const PlaygroundContextValue = createContext({
   isHidden: true,
   setIsHidden: null,
   fontName: "font-Estedad-Medium",
   setFontName: null,
-  theme : "light",
-  setTheme : null
+  theme: "light",
+  setTheme: null,
 });
 
 export function PlaygroundContext({ children }) {
   const [isHidden, setIsHidden] = useState(true);
-  const [fontName, setFontName] = useState("font-Estedad-Medium");
-  const [theme,setTheme] = useState<"light" | "system" | "dark">("light")
+  const [fontName, setFontName] = useState(() => {
+    const savedState = localStorage.getItem("fontName");
+    return savedState ? JSON.parse(savedState) : "font-Estedad-Medium";
+  });
+  const [theme, setTheme] = useState<"light" | "system" | "dark">(() => {
+    const savedState = localStorage.getItem("theme");
+    return savedState ? JSON.parse(savedState) : "light";
+  });
+
+  useEffect(() => {
+    localStorage.setItem("theme", JSON.stringify(theme));
+    localStorage.setItem("fontName", JSON.stringify(fontName));
+  }, [theme, fontName]);
 
   return (
     <PlaygroundContextValue.Provider
-      value={{ isHidden, setIsHidden, fontName, setFontName , theme , setTheme }}
+      value={{ isHidden, setIsHidden, fontName, setFontName, theme, setTheme }}
     >
       {children}
     </PlaygroundContextValue.Provider>
