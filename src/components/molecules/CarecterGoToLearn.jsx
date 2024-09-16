47dc08774f9761b39425b68bf53f9057fd9c9e1b
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-09-16 16:59:28 +0330

    Refactor: Update character positions in CarecterGoToLearn component

--- src/components/molecules/CarecterGoToLearn.jsx
+++ src/components/molecules/CarecterGoToLearn.jsx
@@ -1,21 +1,21 @@
 import CarecterBlue from "../../../public/svgs/caracter2.svg";
 import CarecterOrange from "../../../public/svgs/caracter4.svg";
 import CaracterGreen from "../../../public/svgs/carecter7.svg";
-import CaracterGreentow from "../../../public/svgs/carecter7.svg";
+import CaracterGreentow from "../../../public/svgs/educationvectoryellow.svg";
 
 function CarecterGoToLearn() {
   return (
     <>
-      <span className="absolute left-[10%] -z-10 md:right-[65%] top-36 md:top-10">
+      <span className="absolute left-[10%] -z-10 md:right-[60%] top-36 md:top-60">
         <CarecterBlue />
       </span>
-      <span className="absolute max-md:hidden -z-10 md:right-[55%] top-72">
+      <span className="absolute right-[10%] -z-10 md:right-[35%] top-72 md:top-64">
         <CarecterOrange />
       </span>
-      <span className="absolute max-md:hidden -z-10 md:right-[30%] top-64">
+      <span className="absolute max-md:hidden -z-10 md:right-[18%] top-[12rem] md:top-52">
         <CaracterGreentow />
       </span>
-      <span className="absolute right-[10%] -z-10 md:right-[18%] top-[12rem] md:top-7">
+      <span className="absolute max-md:hidden right-[10%] -z-10 md:right-[50%] top-72">
         <CaracterGreen />
       </span>
     </>
