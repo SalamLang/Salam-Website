6f9016e4ff762667a69f23ab6a20e973cc10ecfd
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-09-17 22:11:34 +0330

    Refactor Accordion component to add dark mode support

--- src/components/molecules/Accordion.jsx
+++ src/components/molecules/Accordion.jsx
@@ -9,7 +9,7 @@ const Accordion = ({ number, title, content, index, active, handleToggle }) => {
         className="cursor-pointer p-4 flex justify-between items-center"
         onClick={handleToggle}
       >
-        <h3 className="text-lg md:text-2xl font-Estedad-Medium flex justify-start items-center gap-5">
+        <h3 className="text-lg md:text-2xl font-Estedad-Medium flex justify-start items-center gap-5 dark:text-white transition-colors">
           <span className="bg-bg-main w-[40px] h-[40px] md:w-[60px] md:h-[60px] rounded-xl md:rounded-2xl text-white text-lg md:text-3xl flex items-center justify-center">
             {number}
           </span>
@@ -26,7 +26,7 @@ const Accordion = ({ number, title, content, index, active, handleToggle }) => {
             height={6}
             alt="arrow-icon"
             onClick={handleToggle}
-            className={"cursor-pointer max-md:scale-75"}
+            className={"cursor-pointer max-md:scale-75 dark:invert"}
           />
         </motion.span>
       </div>
@@ -38,7 +38,7 @@ const Accordion = ({ number, title, content, index, active, handleToggle }) => {
         className="overflow-hidden w-full max-w-[60rem]"
       >
         <div className="p-4 pt-0 w-10/12 mx-auto mb-4">
-          <p className="max-md:text-sm">{content}</p>
+          <p className="max-md:text-sm dark:text-gray-300 transition-colors">{content}</p>
         </div>
       </motion.div>
       <hr className={"border-border-grey2 border-b-2 relative opacity-60"} />
