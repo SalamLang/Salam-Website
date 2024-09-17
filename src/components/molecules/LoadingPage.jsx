badac918833baab4a77ef80d750e039172c3c98b
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-09-17 10:00:34 +0330

    feat: Add loading component to LoadingPage with logo and title
    Add a loading component to the LoadingPage that includes a logo and a title in the Persian language.

--- src/components/molecules/LoadingPage.jsx
+++ src/components/molecules/LoadingPage.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { LoaderIcon } from "react-hot-toast";
+import Logo from "../atoms/Logo";
 
 export default function LoadingPage() {
   return (
-    <div className="fixed w-full h-full bg-white/50 backdrop-blur-xl inset-0 z-[101] flex justify-center items-center overflow-hidden">
-      <LoaderIcon />
+    <div className="fixed w-full h-full bg-white/50 backdrop-blur-xl inset-0 z-[101] flex flex-col gap-3 justify-center items-center overflow-hidden">
+      <div className="w-full h-auto p-2 flex flex-col justify-center items-center gap-2">
+        <Logo />
+        <h1 className="text-base md:text-lg font-Estedad-Bold text-bg-main">
+          زبان برنامه نویسی سلام
+        </h1>
+      </div>
+      <LoaderIcon className="!w-7 !h-7 max-md:!w-5 max-md:!h-5" />
     </div>
   );
 }
