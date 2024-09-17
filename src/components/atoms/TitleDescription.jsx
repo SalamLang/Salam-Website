2305311e47f630c054380e5b4a0b206f13ca6a4d
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-09-17 15:40:43 +0330

    Refactor TitleDescription component to add dark mode support

--- src/components/atoms/TitleDescription.jsx
+++ src/components/atoms/TitleDescription.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 
-const TitleDescription = ({ title, description , headerClass,descClass , className }) => {
+const TitleDescription = ({
+  title,
+  description,
+  headerClass,
+  descClass,
+  className,
+}) => {
   return (
-    <div className={`flex items-center justify-center flex-col gap-6 z-50 ${className || ""}`}>
-      <h1 className={`text-xl md:text-2xl lg:text-3xl font-Estedad-SemiBold text-title text-center ${headerClass || ""}`}>
+    <div
+      className={`flex items-center justify-center flex-col gap-6 z-50 ${
+        className || ""
+      }`}
+    >
+      <h1
+        className={`text-xl md:text-2xl lg:text-3xl font-Estedad-SemiBold text-title dark:!text-white transition-colors text-center ${
+          headerClass || ""
+        }`}
+      >
         {title}
       </h1>
-      <p className={`text-description text-center font-Estedad-Medium text-gray-500 text-xs ${descClass || ""}`}>
+      <p
+        className={`text-description text-center font-Estedad-Medium text-gray-500 dark:!text-gray-300/70 transition-colors text-xs ${
+          descClass || ""
+        }`}
+      >
         {description}
       </p>
     </div>
