1b08f720b4f6f18582ae56e041dac4c95f725c6a
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-09-17 21:07:12 +0330

    Refactor LoadingPage component to add dark mode support

--- src/components/molecules/LoadingPage.jsx
+++ src/components/molecules/LoadingPage.jsx
@@ -4,7 +4,7 @@ import Logo from "../atoms/Logo";
 
 export default function LoadingPage() {
   return (
-    <div className="fixed w-full h-full bg-white/50 backdrop-blur-xl inset-0 z-[101] flex flex-col gap-3 justify-center items-center overflow-hidden">
+    <div className="fixed w-full h-full bg-white/50 dark:bg-gray-900/50 backdrop-blur-xl inset-0 z-[101] flex flex-col gap-3 justify-center items-center overflow-hidden">
       <div className="w-full h-auto p-2 flex flex-col justify-center items-center gap-2">
         <Logo />
         <h1 className="text-base md:text-lg font-Estedad-Bold text-bg-main">
