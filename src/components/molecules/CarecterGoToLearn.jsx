d807654f00bc522d451119c824db400fe3c82851
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-09-17 22:11:43 +0330

    Refactor CarecterGoToLearn component to add dark mode support

--- src/components/molecules/CarecterGoToLearn.jsx
+++ src/components/molecules/CarecterGoToLearn.jsx
@@ -6,16 +6,16 @@ import CaracterGreentow from "../../../public/svgs/educationvectoryellow.svg";
 function CarecterGoToLearn() {
   return (
     <>
-      <span className="absolute left-[10%] -z-10 md:right-[60%] top-36 md:top-60">
+      <span className="absolute left-[10%] -z-10 dark:z-10 md:right-[65%] top-36 md:top-60">
         <CarecterBlue />
       </span>
-      <span className="absolute right-[10%] -z-10 md:right-[35%] top-72 md:top-64">
+      <span className="absolute right-[10%] -z-10 dark:z-10 md:right-[40%] top-72 md:top-64">
         <CarecterOrange />
       </span>
-      <span className="absolute max-md:hidden -z-10 md:right-[18%] top-[12rem] md:top-52">
+      <span className="absolute max-md:hidden -z-10 dark:z-10 md:right-[23%] top-[12rem] md:top-52">
         <CaracterGreentow />
       </span>
-      <span className="absolute max-md:hidden right-[10%] -z-10 md:right-[50%] top-72">
+      <span className="absolute max-md:hidden right-[10%] -z-10 dark:z-10 md:right-[55%] top-72">
         <CaracterGreen />
       </span>
     </>
