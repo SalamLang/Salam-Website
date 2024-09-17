dcb6ceeee5588b70977482b47f37d622a6e1be07
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-09-17 22:12:25 +0330

    Refactor WantSupporter component to use TitleDescription atom

--- src/components/templates/WantSupporter.jsx
+++ src/components/templates/WantSupporter.jsx
@@ -2,17 +2,16 @@ import React from "react";
 import { Button } from "../common/Button";
 import SupporterIcon from "../../../public/images/supporter.png";
 import Image from "next/image";
+import TitleDescription from "../atoms/TitleDescription";
 
 export default function WantSupporter() {
   return (
     <div className="w-full min-h-[300px] md:min-h-[500px] h-full flex flex-col gap-4 md:gap-7 justify-center items-center">
-      <h1 className="w-full text-2xl md:text-4xl font-Estedad-SemiBold text-center">
-        من میخوام حامی بشم چیکار کنم ؟
-      </h1>
-      <p className="text-xs sm:text-sm font-Estedad-Regular opacity-70 md:opacity-60 w-1/2 text-center">
-        برای حامی شدم روی دکمه ی زیر بزنید و ما چندتا حامی هم داریم میتونی
-        ببینید به سادگی بفرما !
-      </p>
+      <TitleDescription
+        title="من میخوام حامی بشم چیکار کنم ؟"
+        description="برای حامی شدم روی دکمه ی زیر بزنید و ما چندتا حامی هم داریم میتونی
+        ببینید به سادگی بفرما !"
+      />
       <Button
         intent="crimson"
         link="/supporter"
