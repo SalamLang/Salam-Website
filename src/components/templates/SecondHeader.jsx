ea30a7dd8b2665de9ee22b344fab586c7b27c718
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-09-17 15:41:46 +0330

    Refactor SecondHeader component to improve dark mode support and add dynamic width

--- src/components/templates/SecondHeader.jsx
+++ src/components/templates/SecondHeader.jsx
@@ -5,20 +5,15 @@ import Hamberger from "../atoms/Hamberger";
 import Logo from "@/components/atoms/Logo";
 import ToggleMenuProvider from "@/utils/contexts/ToggleMenu";
 import SecondMenuList from "../molecules/SecondMenuList";
-import { LayoutContext } from "@/utils/contexts/LayoutProvider";
-import { useContext } from "react";
 
 function SecondHeader({ className }) {
-  const { isDark } = useContext(LayoutContext);
   return (
     <div
-      className={`relative w-full shadow-2xl transition-colors shadow-black/[0.05] ${
-        isDark && "dark:shadow-gray-600/[0.05] dark:bg-dark dark:text-white"
-      } ${className}`}
+      className={`relative w-full shadow-2xl transition-colors shadow-black/[0.05] dark:shadow-gray-600/[0.05] dark:bg-gray-900/80 dark:text-white ${className}`}
     >
       <header
         className={
-          "md:shadow-my3 lg:shadow-my2 bg-transparent relative w-full px-4 py-2 md:h-[80px] xl:h-[100px] flex items-center justify-center"
+          "md:shadow-my3 lg:shadow-my2 bg-transparent relative w-full px-4 py-2 md:h-[80px] xl:h-[100px] flex items-center justify-center gap-3"
         }
       >
         <SecondMenuList />
