1cd37978fabd52106692da5c7831c70cb54492c2
John Bampton <jbampton@users.noreply.github.com>
2024-09-20 22:20:18 +1000

    Fix Jest tests (#203)

--- src/components/common/__tests__/badge.test.jsx
+++ src/components/common/__tests__/badge.test.jsx
@@ -2,16 +2,34 @@
 
 import React from 'react';
 import renderer from 'react-test-renderer';
-import Badge from '../Badge';
+import {Badge} from '../Badge';
 
 it('renders correctly', () => {
   const tree = renderer
-    .create('<Badge className="primary">آنلاین کار کن</Badge>').toJSON();
+    .create(<Badge className="primary">آنلاین کار کن</Badge>).toJSON();
   expect(tree).toMatchSnapshot();
 });
 
 it('renders correctly', () => {
   const tree = renderer
-    .create('<Badge className="primary medium full">آنلاین کار کن</Badge>').toJSON();
+    .create(<Badge className="primary medium full">آنلاین کار کن</Badge>).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<Badge intent="success">کوتاه بودن</Badge>).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<Badge intent="warning">زیبا بودن</Badge>).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<Badge intent="danger">فارسی بودن</Badge>).toJSON();
   expect(tree).toMatchSnapshot();
 });
