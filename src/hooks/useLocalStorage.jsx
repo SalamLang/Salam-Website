43beffcf3e32159b2084672e67f55860f8158bb8
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-09-21 13:05:39 +0330

    Refactor useLocalStorage hook to handle server-side rendering

--- src/hooks/useLocalStorage.jsx
+++ src/hooks/useLocalStorage.jsx
@@ -1,33 +1,36 @@
 "use client";
 
-import { useState } from "react"
+import { useState } from "react";
+
+const IS_SERVER = typeof window === "undefined";
 
 const useLocalStorage = (key, initialValue) => {
   const [state, setState] = useState(() => {
     // Initialize the state
     try {
-      const value = window.localStorage.getItem(key)
+      const value = IS_SERVER ? null : window.localStorage.getItem(key);
       // Check if the local storage already has any values,
       // otherwise initialize it with the passed initialValue
-      return value ? JSON.parse(value) : initialValue
+      return value && !IS_SERVER ? JSON.parse(value) : initialValue;
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  })
+  });
 
-  const setValue = value => {
+  const setValue = (value) => {
     try {
       // If the passed value is a callback function,
       //  then call it with the existing state.
-      const valueToStore = value instanceof Function ? value(state) : value
-      window.localStorage.setItem(key, JSON.stringify(valueToStore))
-      setState(value)
+      const valueToStore = value instanceof Function ? value(state) : value;
+      if (IS_SERVER)
+        window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      setState(value);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
-  return [state, setValue]
-}
+  return [state, setValue];
+};
 
-export default useLocalStorage
+export default useLocalStorage;
