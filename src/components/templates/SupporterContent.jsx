3832a12d687cb8d53380668794e19370a97967b0
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-09-22 10:33:29 +0330

    Refactor SupporterContent component to improve layout and add title description

--- src/components/templates/SupporterContent.jsx
+++ src/components/templates/SupporterContent.jsx
@@ -1,28 +1,32 @@
 import React from "react";
 import SupportBox from "../molecules/SupportBox";
+import TitleDescription from "../atoms/TitleDescription";
 
 export default function SupporterContent() {
   return (
-    <div className="relative max-w-7xl w-full h-full min-h-[20rem]">
-      <div className="absolute inset-0 w-full h-full flex max-md:flex-col px-4 justify-between items-center gap-5">
-        <div className="w-auto h-full flex flex-col justify-between gap-4 items-center">
-          <SupportBox intent="liberapay" />
-          <SupportBox intent="issuehunt" />
-          <SupportBox intent="buymeacoffee" />
+    <div className="relative max-w-7xl w-full h-full min-h-[20rem] overflow-hidden">
+      <div className="relative inset-0 w-full h-full flex max-md:flex-col px-4 justify-between items-center max-md:gap-8 gap-5">
+        <div className="w-full h-full flex max-md:flex-col-reverse justify-start items-center max-md:gap-10 gap-5">
+          <div className="w-auto h-full flex flex-col justify-between gap-8 items-center">
+            <SupportBox intent="liberapay" />
+            <SupportBox intent="issuehunt" />
+            <SupportBox intent="buymeacoffee" />
+          </div>
+          <div className="w-full h-full flex justify-center items-center">
+            <div className="max-w-[75%] w-full h-full flex flex-col justify-center items-center gap-3">
+              <TitleDescription
+                title="چرا ما؟"
+                description="لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
+                استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله
+                در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد
+                نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد،
+                کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان"
+                descClass={"dark:!text-white/90"}
+              />
+            </div>
+          </div>
         </div>
-        <div className="max-w-[50%] w-full h-full flex flex-col justify-center items-center gap-3">
-          <h2 className="font-Estedad-SemiBold text-lg md:text-2xl lg:text-3xl dark:text-white">
-            چرا ما؟
-          </h2>
-          <p className="text-xs sm:text-sm md:text-base dark:text-gray-300">
-            لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
-            استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در
-            ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز،
-            و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای
-            زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان
-          </p>
-        </div>
-        <div className="w-auto h-full flex flex-col justify-between gap-4 items-center">
+        <div className="w-1/4 h-full flex flex-col justify-between gap-8 items-center">
           <SupportBox intent="kofi" />
           <SupportBox intent="opencollective" />
           <SupportBox intent="patreon" />
