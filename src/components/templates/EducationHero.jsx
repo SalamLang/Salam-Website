f9360337e3f66b4b6b213f0b738d59f7d19c55cb
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-09-22 15:50:05 +0330

    Refactor EducationHero component to add smooth scrolling to download section

--- src/components/templates/EducationHero.jsx
+++ src/components/templates/EducationHero.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Gif from "@/components/atoms/Gif";
 import CarectersDownload from "@/components/molecules/CarectersDownload";
 
@@ -7,6 +9,14 @@ import React from "react";
 import { Button } from "../common/Button";
 
 function EducationHero() {
+  const handleScrollToSection = () => {
+    const section = document.getElementById("download-section");
+    if (section) {
+      const offsetTop =
+        section.getBoundingClientRect().top + window.scrollY - 120;
+      window.scrollTo({ top: offsetTop, behavior: "smooth" });
+    }
+  };
   return (
     <div className="container flex flex-col items-center justify-center gap-5 w-full">
       <div className="w-auto relative flex items-center justify-center">
@@ -21,7 +31,7 @@ function EducationHero() {
         intent="primary"
         rounded="full"
         className="flex justify-center items-center gap-1 !bg-white !text-text-button-orange2"
-        link="/playground"
+        onClick={handleScrollToSection}
       >
         برو بریم
         <ArrowBottom className="relative top-0.5 scale-90" />
