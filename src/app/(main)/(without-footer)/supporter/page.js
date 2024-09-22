c36192cb7c7a04cc7c3fca25d1e25ea040df511e
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-09-22 15:49:55 +0330

    Refactor page component to add metadata for SEO optimization

--- src/app/(main)/(without-footer)/supporter/page.js
+++ src/app/(main)/(without-footer)/supporter/page.js
@@ -1,6 +1,13 @@
 import SupporterContent from "@/components/templates/SupporterContent";
 import React from "react";
 
+export const metadata = {
+  title: "حامی های زبان سلام - برنامه نویسی سلام",
+  description:
+    "حمایت از سایت زبان سلام از طریق پلتفرم‌های مختلف مانند Liberapay، IssueHunt، BuyMeACoffee، Ko-fi، OpenCollective و Patreon",
+  author: "تیم برنامه نویسی زبان سلام",
+};
+
 export default function page() {
   return (
     <div className="w-full h-full flex-grow flex justify-center items-start md:items-center max-md:py-20">
