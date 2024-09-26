e013b39442ad9b2e86d3e7ac2e482480eecce331
mahaddeseh <mohi18hdadi1400@gmail.com>
2024-09-26 14:12:39 +0330

    chore:add logobox componant

---
+++ src/components/atoms/logoBox.jsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import LogoSvg from "../../../public/svgs/logoOrange.svg";
+import React from 'react'
+
+function LogoBox() {
+    const name="ali"
+  return (
+    <div className="flex items-center justify-center">
+      <div className="w-[120px]">
+        <Link href={"/"}>
+        <LogoSvg/>
+        </Link>
+      
+      </div>
+    </div>
+  )
+}
+
+export default LogoBox
