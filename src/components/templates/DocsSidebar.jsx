4d01a7ecd64ba9a771cf155e6a608b81f289a9da
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-09-28 17:14:36 +0330

    Refactor DocsSidebar component to update styling and add responsiveness

--- src/components/templates/DocsSidebar.jsx
+++ src/components/templates/DocsSidebar.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 function DocsSidebar() {
   return (
-    <div className="flex flex-col h-screen overflow-y-auto justify-start w-1/4 py-10">
+    <div className="!sticky top-0 max-lg:hidden flex flex-col h-[40rem] overflow-y-auto justify-start w-[15rem] py-10 bg-gray-100/20 dark:bg-gray-700/20 backdrop-blur-xl rounded-3xl shadow-2xl shadow-gray-400/50 dark:shadow-gray-700/20">
       <LogoBox/>
       <SidebarMenu/>
     </div>
