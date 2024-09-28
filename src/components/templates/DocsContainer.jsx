4412092d386247d3b875dafe57d19df7a14b7490
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-09-28 17:14:29 +0330

    Refactor DocsContainer component to add a container for documentation content

---
+++ src/components/templates/DocsContainer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const DocsContainer = ({ children }) => {
+  return (
+    <div className="text-white items-center w-full p-4 bg-gray-100/20 flex-grow dark:bg-gray-700/20 backdrop-blur-xl rounded-3xl shadow-2xl shadow-gray-400/50 dark:shadow-gray-700/20"
+    >
+      {children}
+    </div>
+  );
+};
