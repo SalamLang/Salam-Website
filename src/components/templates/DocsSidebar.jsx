aea429bcc7e6f1fae10e91caa6f4c0a6b6c3df02
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-09-30 16:52:04 +0330

    Refactor DocsSidebar component to use framer-motion for animation and update styling

--- src/components/templates/DocsSidebar.jsx
+++ src/components/templates/DocsSidebar.jsx
@@ -1,14 +1,17 @@
+"use client";
+
 import LogoBox from "../atoms/logoBox.jsx";
 import SidebarMenu from "../templates/SidebarMenu.jsx";
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
 
 function DocsSidebar() {
   return (
-    <div className="!sticky top-0 max-lg:hidden flex flex-col h-[40rem] overflow-y-auto justify-start w-[15rem] py-10 bg-gray-100/20 dark:bg-gray-700/20 backdrop-blur-xl rounded-3xl shadow-2xl shadow-gray-400/50 dark:shadow-gray-700/20">
-      <LogoBox/>
-      <SidebarMenu/>
-    </div>
-  )
+    <motion.div className="text-white items-center w-full p-4 bg-gray-100/20 flex-grow dark:bg-gray-700/20 backdrop-blur-xl rounded-3xl shadow-2xl shadow-gray-400/50 dark:shadow-gray-700/20 max-w-[15rem] h-screen !sticky !top-28 py-10">
+      <LogoBox />
+      <SidebarMenu />
+    </motion.div>
+  );
 }
 
-export default DocsSidebar
+export default DocsSidebar;
