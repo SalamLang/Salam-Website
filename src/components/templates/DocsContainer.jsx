cd2785ffea6fe2e716488918fcca9081aec8eaaa
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-09-30 16:51:57 +0330

    Refactor DocsContainer component to accept a className prop

--- src/components/templates/DocsContainer.jsx
+++ src/components/templates/DocsContainer.jsx
@@ -1,8 +1,9 @@
+
 import React from "react";
 
-export const DocsContainer = ({ children }) => {
+export const DocsContainer = ({ children , className }) => {
   return (
-    <div className="text-white items-center w-full p-4 bg-gray-100/20 flex-grow dark:bg-gray-700/20 backdrop-blur-xl rounded-3xl shadow-2xl shadow-gray-400/50 dark:shadow-gray-700/20"
+    <div className={`text-white items-center w-full p-4 bg-gray-100/20 flex-grow dark:bg-gray-700/20 backdrop-blur-xl rounded-3xl shadow-2xl shadow-gray-400/50 dark:shadow-gray-700/20 ${className || ""}`}
     >
       {children}
     </div>
