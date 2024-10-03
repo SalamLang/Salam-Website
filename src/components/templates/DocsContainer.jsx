8175d775e8790015ff169971f3fe5dd92d1b52b3
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-10-03 14:49:39 +0330

    Refactor DocsContainer component to use motion from framer-motion and useContext from react, and add motion animation to the container

--- src/components/templates/DocsContainer.jsx
+++ src/components/templates/DocsContainer.jsx
@@ -1,11 +1,19 @@
+"use client";
 
-import React from "react";
+import React, { useContext } from "react";
+import { motion } from "framer-motion";
+import { DocsContext } from "@/utils/contexts/DocsProvider";
 
-export const DocsContainer = ({ children , className }) => {
+export const DocsContainer = ({ children, className, ...attr }) => {
+  const { isOpenMenu } = useContext(DocsContext);
   return (
-    <div className={`text-white items-center w-full p-4 bg-gray-100/20 flex-grow dark:bg-gray-700/20 backdrop-blur-xl rounded-3xl shadow-2xl shadow-gray-400/50 dark:shadow-gray-700/20 ${className || ""}`}
+    <motion.div
+      className={`text-white items-center transition-all p-4 bg-gray-100/20 flex-grow dark:bg-gray-700/20 backdrop-blur-xl rounded-3xl shadow-2xl shadow-gray-400/50 dark:shadow-gray-700/20 ${
+        className || ""
+      }`}
+      {...attr}
     >
       {children}
-    </div>
+    </motion.div>
   );
 };
