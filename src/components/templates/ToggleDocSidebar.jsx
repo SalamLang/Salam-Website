2f2ef272756399387391c9bcc5a8fcdf928197e0
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-10-03 14:49:56 +0330

    Refactor ToggleDocSidebar component to use motion from framer-motion and useContext from react, and add motion animation to the sidebar toggle button

--- src/components/templates/ToggleDocSidebar.jsx
+++ src/components/templates/ToggleDocSidebar.jsx
@@ -1,12 +1,19 @@
 "use client";
-import React from "react";
+import React, { useContext } from "react";
 import Hamberger from "../../../public/svgs/hambeger.svg";
 import { motion } from "framer-motion";
+import { DocsContext } from "@/utils/contexts/DocsProvider";
 
 export default function ToggleDocSidebar() {
+  const { setIsOpenMenu, isOpenMenu } = useContext(DocsContext);
+  console.log(isOpenMenu);
   return (
-    <motion.div whileHover={{y:-5,}} className="fixed w-14 h-14 z-50 bottom-5 right-7 cursor-pointer bg-bg-main/25 border-2 border-bg-main rounded-full flex justify-center items-center">
-        <Hamberger className="scale-50" />
+    <motion.div
+      whileHover={{ y: -5 }}
+      onClick={() => setIsOpenMenu(!isOpenMenu)}
+      className="fixed w-14 h-14 z-50 bottom-5 right-7 cursor-pointer bg-bg-main/25 border-2 border-bg-main rounded-full flex justify-center items-center"
+    >
+      <Hamberger className="scale-50" />
     </motion.div>
   );
 }
