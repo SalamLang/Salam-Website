3ff29796f7eebb46d57d3c66a90226b990956d9e
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-10-03 14:49:50 +0330

    Refactor DocsSidebar component to use motion from framer-motion and useContext from react, and add motion animation to the sidebar

--- src/components/templates/DocsSidebar.jsx
+++ src/components/templates/DocsSidebar.jsx
@@ -2,15 +2,29 @@
 
 import LogoBox from "../atoms/logoBox.jsx";
 import SidebarMenu from "../templates/SidebarMenu.jsx";
-import React from "react";
-import { motion } from "framer-motion";
+import React, { useContext } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { DocsContext } from "@/utils/contexts/DocsProvider";
 
 function DocsSidebar() {
+  const { isOpenMenu } = useContext(DocsContext);
   return (
-    <motion.div className="text-white items-center w-full p-4 bg-gray-100/20 flex-grow dark:bg-gray-700/20 backdrop-blur-xl rounded-3xl shadow-2xl shadow-gray-400/50 dark:shadow-gray-700/20 max-w-[15rem] h-screen !sticky !top-28 py-10">
-      <LogoBox />
-      <SidebarMenu />
-    </motion.div>
+    <>
+      <AnimatePresence>
+        {isOpenMenu && (
+          <motion.div
+            initial={{ x: "100vw" }}
+            animate={{ x: 0 }}
+            exit={{ x: "100vw" }}
+            transition={{ type: "keyframes" }}
+            className="text-white items-center w-full p-4 bg-gray-100/20 flex-grow dark:bg-gray-700/20 backdrop-blur-xl rounded-3xl shadow-2xl shadow-gray-400/50 dark:shadow-gray-700/20 max-w-[15rem] h-screen !sticky max-md:!fixed inset-0 max-md:!top-0 !z-40 !top-28 py-10"
+          >
+            <LogoBox />
+            <SidebarMenu />
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </>
   );
 }
 
