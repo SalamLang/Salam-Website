e88ab7503e7f9da6717a9cbb898ecceac74c9ede
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-10-05 15:44:05 +0330

    Refactor page component to add blue and green boxes for learning content

--- src/app/(main)/(without-footer)/learn/page.jsx
+++ src/app/(main)/(without-footer)/learn/page.jsx
@@ -1,6 +1,8 @@
 import TitleDescription from "@/components/atoms/TitleDescription";
 import React from "react";
 import DoobleEmoji from "../../../../../public/svgs/dooble-emoji.svg";
+import { Button } from "@/components/common/Button";
+import { BoxLearn } from "@/components/molecules/BoxLearn";
 
 export const metadata = {
   title: "اسناد زبان سلام - برنامه نویسی سلام",
@@ -11,7 +13,7 @@ export const metadata = {
 
 export default function page() {
   return (
-    <div className="w-full h-full flex-grow flex p-6 flex-col justify-center items-center gap-2 sm:gap-4 md:gap-6 lg:gap-14 xl:gap-16 overflow-hidden">
+    <div className="w-full h-full flex-grow flex p-10 flex-col justify-center items-center gap-2 sm:gap-4 md:gap-6 lg:gap-14 xl:gap-16 overflow-hidden">
       <TitleDescription
         title="فهرست متن های زبان سلام"
         description="قسمت اسناد اولین زبان برنامه نویسی به نام سلام خوش اومدید"
@@ -20,6 +22,10 @@ export default function page() {
       />
       <div className="w-auto h-auto scale-50 min-h-[100px] md:scale-75 lg:scale-90 xl:scale-100">
         <DoobleEmoji />
+      </div>
+      <div className="w-full h-full max-w-[900px] flex max-md:flex-col justify-center items-center gap-4">
+        <BoxLearn type_color="blue" />
+        <BoxLearn type_color="green" />
       </div>
     </div>
   );
