c915ec3b643982d7d54d5a0fd42e414d837473b1
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-10-09 11:46:29 +0330

    (chore) update nextjsconfig

--- next.config.mjs
+++ next.config.mjs
@@ -19,6 +19,11 @@ const nextConfig = {
       },
     ],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: false,
+  },
   headers: async () => [
     {
       source: '/(.*)',
