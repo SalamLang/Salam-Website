4163bf682bba120202e125aaa570552d15430f5f
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-10-10 17:01:04 +0330

    Refactor MarkDownDocsContent component to add custom ID to h1 elements

--- src/components/templates/MarkDownDocsContent.jsx
+++ src/components/templates/MarkDownDocsContent.jsx
@@ -4,5 +4,24 @@ import Markdown from "markdown-to-jsx";
 import React from "react";
 
 export default function MarkDownDocsContent({ mark_down }) {
-  return <Markdown>{mark_down}</Markdown>;
+  return (
+    <Markdown
+      options={{
+        overrides: {
+          h1: {
+            component: ({ children, ...props }) => {
+              const id = children.toString();
+              return (
+                <h1 {...props} id={id}>
+                  {children}
+                </h1>
+              );
+            },
+          },
+        },
+      }}
+    >
+      {mark_down}
+    </Markdown>
+  );
 }
