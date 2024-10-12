625bde6a4e29f2d69bdc9fdc19478925e35fe8bb
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-10-12 17:59:51 +0330

    Refactor DocsSidebar component to add scroll behavior and fixed positioning

--- src/components/templates/DocsSidebar.jsx
+++ src/components/templates/DocsSidebar.jsx
@@ -2,27 +2,51 @@
 
 import LogoBox from "../atoms/logoBox.jsx";
 import SidebarMenu from "../templates/SidebarMenu.jsx";
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import { DocsContext } from "@/utils/contexts/DocsProvider";
 
 function DocsSidebar() {
   const { isOpenMenu } = useContext(DocsContext);
+  const [isSpaceTop, setIsSpaceTop] = useState(false);
+  const [fixedMenuHeight, setFixedMenuHeight] = useState(0);
+
+  useEffect(() => {
+    const fixedMenu = document.getElementById("second-header");
+
+    const handleScroll = () => {
+      if (fixedMenu) {
+        setFixedMenuHeight(fixedMenu.clientHeight);
+      }
+
+      if (window.scrollY > 150) {
+        setIsSpaceTop(true);
+      } else {
+        setIsSpaceTop(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <>
       <AnimatePresence>
-        {isOpenMenu && (
           <motion.div
-            initial={{ x: "100vw" }}
-            animate={{ x: 0 }}
-            exit={{ x: "100vw" }}
-            transition={{ type: "keyframes" }}
-            className="text-white !overflow-auto orange-scrollbar items-center w-full p-4 bg-gray-100/20 flex-grow dark:bg-gray-700/20 backdrop-blur-xl rounded-3xl shadow-2xl shadow-gray-400/50 dark:shadow-gray-700/20 max-w-[15rem] h-screen !sticky max-md:!fixed inset-0 max-md:!top-0 !z-40 !top-28 py-10"
+            initial={{ x: "100vw", y: 0 }}
+            animate={{ x: isOpenMenu ? 0 : "100vw", y: isSpaceTop ? fixedMenuHeight : 0 }}
+            exit={{ x: "100vw", y: 0 }}
+            transition={{ type: "keyframes" , duration:0.1 }}
+            className="text-white !overflow-auto transition-all orange-scrollbar items-center w-full p-4 bg-gray-100/20 flex-grow dark:bg-gray-700/20 backdrop-blur-xl rounded-3xl shadow-2xl shadow-gray-400/50 dark:shadow-gray-700/20 max-w-[15rem] h-screen !sticky max-md:!fixed inset-0 !z-50 py-10"
+            id="sidebar"
           >
             <LogoBox />
             <SidebarMenu />
           </motion.div>
-        )}
       </AnimatePresence>
     </>
   );
