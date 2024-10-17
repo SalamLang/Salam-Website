ed929bc1bd6312809f04d7a0c4f4492ffe99d158
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-10-17 17:39:57 +0330

    Refactor DocsSidebar component to remove unused state variables and useEffect

--- src/components/templates/DocsSidebar.jsx
+++ src/components/templates/DocsSidebar.jsx
@@ -8,44 +8,43 @@ import { DocsContext } from "@/utils/contexts/DocsProvider";
 
 function DocsSidebar() {
   const { isOpenMenu } = useContext(DocsContext);
-  const [isSpaceTop, setIsSpaceTop] = useState(false);
-  const [fixedMenuHeight, setFixedMenuHeight] = useState(0);
+  // const [isSpaceTop, setIsSpaceTop] = useState(false);
+  // const [fixedMenuHeight, setFixedMenuHeight] = useState(0);
 
-  useEffect(() => {
-    const fixedMenu = document.getElementById("second-header");
+  // useEffect(() => {
+  //   const fixedMenu = document.getElementById("second-header");
 
-    const handleScroll = () => {
-      if (fixedMenu) {
-        setFixedMenuHeight(fixedMenu.clientHeight);
-      }
+  //   const handleScroll = () => {
+  //     if (fixedMenu) {
+  //       setFixedMenuHeight(fixedMenu.clientHeight);
+  //     }
 
-      if (window.scrollY > 150) {
-        setIsSpaceTop(true);
-      } else {
-        setIsSpaceTop(false);
-      }
-    };
+  //     if (window.scrollY > 150) {
+  //       setIsSpaceTop(true);
+  //     } else {
+  //       setIsSpaceTop(false);
+  //     }
+  //   };
 
-    window.addEventListener("scroll", handleScroll);
+  //   window.addEventListener("scroll", handleScroll);
 
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
+  //   return () => {
+  //     window.removeEventListener("scroll", handleScroll);
+  //   };
+  // }, []);
 
   return (
     <>
       <AnimatePresence>
         {isOpenMenu && (
           <motion.div
-            initial={{ x: "100vw", y: 0 }}
+            initial={{ x: "100vw" }}
             animate={{
               x: isOpenMenu ? 0 : "100vw",
-              y: isSpaceTop ? fixedMenuHeight : 0,
             }}
-            exit={{ x: "100vw", y: 0 }}
+            exit={{ x: "100vw" }}
             transition={{ type: "keyframes", duration: 0.1 }}
-            className="text-white !overflow-auto transition-all orange-scrollbar items-center w-full p-4 bg-gray-100/20 flex-grow dark:bg-gray-700/20 backdrop-blur-xl rounded-3xl shadow-2xl shadow-gray-400/50 dark:shadow-gray-700/20 max-w-[15rem] h-screen !sticky max-md:!fixed inset-0 !z-50 py-10"
+            className="text-white min-w-[15rem] !overflow-auto transition-all orange-scrollbar items-center w-full p-4 bg-gray-100/20 flex-grow dark:bg-gray-700/20 backdrop-blur-xl rounded-3xl shadow-2xl shadow-gray-400/50 dark:shadow-gray-700/20 max-w-[15rem] h-screen !sticky max-md:!fixed inset-0 !z-50 py-10"
             id="sidebar"
           >
             <LogoBox />
