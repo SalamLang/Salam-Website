fba65512a81e14f36e7c86bf1c07db42edd9ffd4
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-11-14 15:42:59 +0330

    Refactor DocsMainContainer to simplify className by removing unnecessary width utility

--- src/components/templates/DocsMainContainer.jsx
+++ src/components/templates/DocsMainContainer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function DocsMainContainer({ children }) {
   return (
     <div
-      className="w-full px-5 py-5 h-full flex gap-5 flex-grow"
+      className="px-5 py-5 h-full flex"
     >
       {children}
     </div>
