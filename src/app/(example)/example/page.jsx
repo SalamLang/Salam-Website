743a17b584e04fe3f65f6a493ce970a675b45816
Ali Josheghani <132874234+ali-josheghani@users.noreply.github.com>
2024-12-15 00:14:43 +0330

    feat(page.jsx): add page for new path or route for example route

---
+++ src/app/(example)/example/page.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const metadata = {
+  title: "زبان برنامه نویسی سلام - برنامه نویسی برای همه",
+  description:
+    "این صفحه به معرفی زبان برنامه‌نویسی سلام می‌پردازد که هدف آن ساده‌سازی برنامه‌نویسی برای همه افراد است.",
+  author: "تیم برنامه نویسی زبان سلام",
+};
+
+export default function page() {
+  return (
+    <main className="flex min-h-screen flex-col max-md:gap-y-10 gap-y-20 items-center justify-start">
+        s
+    </main>
+  );
+}
