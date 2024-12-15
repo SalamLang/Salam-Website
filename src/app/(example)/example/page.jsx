5eec811cd770b9e337b25928f38f2b21a2a6e9e9
Ali Josheghani <ali.josheghani2005@gmail.com>
2024-12-15 13:01:31 +0330

    feat(page.jsx): add image component to example page

--- src/app/(example)/example/page.jsx
+++ src/app/(example)/example/page.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 export const metadata = {
@@ -10,7 +11,9 @@ export const metadata = {
 export default function page() {
   return (
     <main className="flex min-h-screen flex-col max-md:gap-y-10 gap-y-20 items-center justify-start">
-        s
+        <div className="w-full h-full p-2 flex justify-center items-center">
+          <Image width="600" height="600" src="/images/logo-vector-custom-salam.png" alt="logo" />
+        </div>
     </main>
   );
 }
