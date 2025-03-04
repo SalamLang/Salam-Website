a042fa796051a4886b6f902f8be5786fab737d01
Ali Josheghani <ali.josheghani2005@gmail.com>
2025-03-04 21:34:19 +0330

    feat: add metadata for Salam programming language page

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -6,6 +6,33 @@ import { Toaster } from "react-hot-toast";
 import { LayoutProvider } from "@/utils/contexts/LayoutProvider";
 import ProgressBar from "@/components/templates/ProgressBar";
 
+export const metadata = {
+  title: "زبان برنامه‌نویسی سلام",
+  description:
+    "این صفحه به معرفی زبان برنامه‌نویسی سلام می‌پردازد که هدف آن ساده‌سازی برنامه‌نویسی برای همه افراد است.",
+  openGraph: {
+    title: "زبان برنامه‌نویسی سلام - برنامه نویسی برای همه",
+    description:
+      "این صفحه به معرفی زبان برنامه‌نویسی سلام می‌پردازد که هدف آن ساده‌سازی برنامه‌نویسی برای همه افراد است.",
+    images: [
+      {
+        url: "https://salamlang.ir/screenshot-desktop.png", // image link
+        width: 1200,
+        height: 630,
+      },
+    ],
+    url: "https://salamlang.ir",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "زبان برنامه‌نویسی سلام - برنامه نویسی برای همه",
+    description:
+      "این صفحه به معرفی زبان برنامه‌نویسی سلام می‌پردازد که هدف آن ساده‌سازی برنامه‌نویسی برای همه افراد است.",
+    images: ["https://salamlang.ir/screenshot-desktop.png"], // image link
+  },
+};
+
 function RootLayout({ children }) {
   // let theme = "light"; // default
   // if (typeof window === "undefined") {
