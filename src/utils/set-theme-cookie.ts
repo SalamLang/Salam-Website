92873e148031bf25f0178adb465ffa7ddda59ae8
Ali Josheghani <ali.josheghani2005@gmail.com>
2025-03-06 18:24:56 +0330

    refactor(theme): remove setThemeCookie function

--- src/utils/set-theme-cookie.ts
+++
@@ -1,8 +0,0 @@
-function setThemeCookie(theme: "light" | "dark") {
-  document.cookie = `theme=${theme}; path=/; max-age=31536000`;
-  const root = document.documentElement;
-  root.classList.remove("light", "dark");
-  root.classList.add(theme);
-}
-
-export default setThemeCookie;
