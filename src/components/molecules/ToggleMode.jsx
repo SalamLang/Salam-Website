e8c0474bef204f82b40235a0da546cde7ad6bdd5
Ali Josheghani <ali.josheghani2005@gmail.com>
2025-03-08 18:17:31 +0330

    feat(toggle): implement cookie management for theme toggling

--- src/components/molecules/ToggleMode.jsx
+++ src/components/molecules/ToggleMode.jsx
@@ -3,19 +3,23 @@
 import React, { useContext, useEffect, useState } from "react";
 import { LayoutContext } from "@/utils/contexts/LayoutProvider";
 import SunMoonToggle from "../templates/SunMoonToggle";
+import useCookie from "@/hooks/useCookie";
 
 export default function ToggleMode() {
   const { setTheme, isToggle, setIsToggle } = useContext(LayoutContext);
+
+  const { handleSetCookie } = useCookie("theme");
+
   const handleToggle = () => {
     setIsToggle(!isToggle);
     if (isToggle) {
       setTheme("light");
+      handleSetCookie("light");
     } else {
       setTheme("dark");
+      handleSetCookie("dark");
     }
   };
 
-  return (
-    <SunMoonToggle isToggle={isToggle} onClick={handleToggle} />
-  );
+  return <SunMoonToggle isToggle={isToggle} onClick={handleToggle} />;
 }
