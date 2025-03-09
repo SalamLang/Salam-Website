c4517995b3201727d1d74512b197e31507cb42a8
Ali Josheghani <ali.josheghani2005@gmail.com>
2025-03-09 13:06:38 +0330

    feat(cookies): enhance createCookie function to accept additional options

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -2,17 +2,26 @@
 
 import { cookies } from "next/headers";
 
-export async function createCookie(name: string, value: string) {
+export async function createCookie(
+  name: string,
+  value: string,
+  options: {
+    maxAge?: number;
+    expires?: Date;
+    httpOnly?: boolean;
+    path?: string;
+  } = {}
+) {
   const cookieStore = await cookies();
   cookieStore.set({
-    name: name,
-    value: value,
+    name,
+    value,
     httpOnly: true,
     path: "/",
+    ...options,
   });
 }
-
-export async function getCookie(name: string) {
+export async function getCookie(name: string): Promise<string | undefined> {
   const cookieStore = await cookies();
-  return cookieStore.get(name).value;
+  return cookieStore.get(name)?.value;
 }
