b74a42c6dc737341bd0f5f0938737eedcafd3660
Ali Josheghani <ali.josheghani2005@gmail.com>
2025-03-09 13:06:53 +0330

    feat(loading): update LoadingPage component to use a loader span and improve styling

--- src/components/molecules/LoadingPage.jsx
+++ src/components/molecules/LoadingPage.jsx
@@ -7,11 +7,11 @@ export default function LoadingPage() {
     <div className="absolute w-full h-full bg-white dark:bg-gray-900 backdrop-blur-xl inset-0 z-[101] flex flex-col gap-3 justify-center items-center overflow-hidden">
       <div className="w-full h-auto p-2 flex flex-col justify-center items-center gap-2">
         <Logo />
-        <h1 className="text-base md:text-lg font-Estedad-Bold text-bg-main">
+        <h1 className="text-base md:text-lg font-Estedad-Bold !text-bg-main">
           زبان برنامه نویسی سلام
         </h1>
       </div>
-      <LoaderIcon className="!w-7 !h-7 max-md:!w-5 max-md:!h-5" />
+      <span className="loader"></span>
     </div>
   );
 }
