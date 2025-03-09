22614c233622566272019137f049ba6faf272cc2
Ali Josheghani <ali.josheghani2005@gmail.com>
2025-03-09 16:01:52 +0330

    feat(MarkDownDocsContent): migrate to ReactMarkdown and enhance heading components

--- src/components/templates/MarkDownDocsContent.jsx
+++ src/components/templates/MarkDownDocsContent.jsx
@@ -1,57 +1,38 @@
 "use client";
 
-import Markdown from "markdown-to-jsx";
+import ReactMarkdown from "react-markdown";
 import React from "react";
+import remarkGfm from "remark-gfm";
+import rehypeRaw from "rehype-raw";
 
+const createHeading =
+  (Tag) =>
+  ({ children, node, ...props }) => {
+    const id = children.toString();
+    return (
+      <Tag {...props} id={id}>
+        {children}
+      </Tag>
+    );
+  };
 export default function MarkDownDocsContent({ mark_down }) {
   return (
-    <Markdown
-      options={{
-        overrides: {
-          h1: {
-            component: ({ children, ...props }) => {
-              const id = children.toString();
-              return (
-                <h1 {...props} id={id}>
-                  {children}
-                </h1>
-              );
-            },
-          },
-          h2: {
-            component: ({ children, ...props }) => {
-              const id = children.toString();
-              return (
-                <h2 {...props} id={id}>
-                  {children}
-                </h2>
-              );
-            },
-          },
-          h3: {
-            component: ({ children, ...props }) => {
-              const id = children.toString();
-              return (
-                <h3 {...props} id={id}>
-                  {children}
-                </h3>
-              );
-            },
-          },
-          h4: {
-            component: ({ children, ...props }) => {
-              const id = children.toString();
-              return (
-                <h4 {...props} id={id}>
-                  {children}
-                </h4>
-              );
-            },
-          },
-        },
-      }}
+    <div
+      className="markdown-body"
+      style={{ direction: "rtl", textAlign: "right" }}
     >
-      {mark_down}
-    </Markdown>
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm]}
+        rehypePlugins={[rehypeRaw]}
+        components={{
+          h1: createHeading("h1"),
+          h2: createHeading("h2"),
+          h3: createHeading("h3"),
+          h4: createHeading("h4"),
+        }}
+      >
+        {mark_down}
+      </ReactMarkdown>
+    </div>
   );
 }
