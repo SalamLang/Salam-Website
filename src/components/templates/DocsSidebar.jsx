76c2f97210177f3ce15e981f1e6f41bcc734a329
Ali Josheghani <ali.josheghani2005@gmail.com>
2025-03-09 16:01:47 +0330

    feat(DocsSidebar): refine sidebar styles for improved responsiveness and animation

--- src/components/templates/DocsSidebar.jsx
+++ src/components/templates/DocsSidebar.jsx
@@ -10,8 +10,8 @@ function DocsSidebar() {
   const { isOpenMenu } = useContext(DocsContext);
 
   const sidebarVariants = {
-    open: { width: 400, x:0 },
-    closed: { width: 0 , x:"100vw" },
+    open: { width: 400, x: 0 },
+    closed: { width: 0, x: "100vw" },
   };
 
   return (
@@ -24,12 +24,17 @@ function DocsSidebar() {
             animate="open"
             exit="closed"
             variants={sidebarVariants}
-            className="md:ml-5 max-md:fixed max-md:inset-0 z-50"
-            transition={{ type: "keyframes", stiffness: 260, damping: 20 , duration: 0.3}}
+            className="md:ml-5 max-lg:fixed max-lg:inset-0 z-50"
+            transition={{
+              type: "keyframes",
+              stiffness: 260,
+              damping: 20,
+              duration: 0.3,
+            }}
             style={{ minHeight: "100vh" }}
           >
             <div
-              className="text-white !flex-nowrap w-full !text-nowrap !overflow-auto transition-all orange-scrollbar items-center p-4 bg-gray-100/20 dark:bg-gray-700/20 backdrop-blur-xl md:rounded-3xl shadow-2xl shadow-gray-400/50 dark:shadow-gray-700/20 h-screen !sticky max-md:!max-w-[15rem] max-md:!fixed inset-0 !z-50 py-10"
+              className="text-white !flex-nowrap w-full !text-nowrap !overflow-auto transition-all orange-scrollbar items-center p-4 bg-gray-100/20 dark:bg-gray-700/20 backdrop-blur-xl md:rounded-3xl shadow-2xl shadow-gray-400/50 dark:shadow-gray-700/20 h-screen !sticky max-lg:!max-w-[20rem] max-md:!fixed inset-0 !z-50 py-10"
               id="sidebar"
             >
               <LogoBox />
