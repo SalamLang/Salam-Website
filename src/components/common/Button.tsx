34a5f33701f4ca0e0a9756c93eca5cfe74197a2c
Ali Josheghani <ali.josheghani2005@gmail.com>
2025-03-11 18:50:19 +0330

    feat(Button): enhance button component with size and rounded variants

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -2,55 +2,63 @@ import React, { HTMLAttributes } from "react";
 import { cva, type VariantProps } from "class-variance-authority";
 import Link from "next/link";
 
-const button = cva("hover:scale-105 transition-all disabled:opacity-70", {
-  variants: {
-    intent: {
-      orange: [
-        "bg-bg-section",
-        "text-white",
-        "border-transparent",
-        "font-Estedad-Regular",
-      ],
-      primary: [
-        "bg-primary",
-        "text-white",
-        "border-transparent",
-        "font-Estedad-Regular",
-      ],
-      crimson: [
-        "bg-crimson",
-        "text-white",
-        "border-transparent",
-        "font-Estedad-Regular",
-      ],
+const button = cva(
+  "hover:scale-105 transition-all disabled:opacity-70 cursor-pointer",
+  {
+    variants: {
+      intent: {
+        orange: [
+          "bg-bg-section",
+          "text-white",
+          "border-transparent",
+          "font-Estedad-Regular",
+        ],
+        primary: [
+          "bg-primary",
+          "text-white",
+          "border-transparent",
+          "font-Estedad-Regular",
+        ],
+        crimson: [
+          "bg-crimson",
+          "text-white",
+          "border-transparent",
+          "font-Estedad-Regular",
+        ],
+      },
+      size: {
+        small: ["text-sm", "pt-1", "pb-1.5", "px-2"],
+        medium: ["text-base md:text-lg", "pt-1.5", "pb-2", "px-4"],
+        large: [
+          "max-sm:text-sm sm:text-base md:text-xl",
+          "max-sm:pt-1 sm:pt-1.5 md:pt-2",
+          "max-sm:pb-1.5 sm:pb-2 md:pb-3",
+          "max-sm:px-3 sm:px-5 md:px-7",
+        ],
+      },
+      rounded: {
+        small: ["rounded-sm"],
+        medium: ["rounded-md"],
+        large: ["rounded-lg"],
+        full: ["rounded-full"],
+        none: ["rounded-none"],
+      },
     },
-    size: {
-      small: ["text-sm", "pt-1", "pb-1.5", "px-2"],
-      medium: ["text-base md:text-lg", "pt-1.5", "pb-2", "px-4"],
-      large: [
-        "max-sm:text-sm sm:text-base md:text-xl",
-        "max-sm:pt-1 sm:pt-1.5 md:pt-2",
-        "max-sm:pb-1.5 sm:pb-2 md:pb-3",
-        "max-sm:px-3 sm:px-5 md:px-7",
-      ],
+    compoundVariants: [
+      {
+        intent: "orange",
+        size: "medium",
+        rounded: "medium",
+        class: "uppercase",
+      },
+    ],
+    defaultVariants: {
+      intent: "orange",
+      size: "medium",
+      rounded: "medium",
     },
-    rounded: {
-      small: ["rounded-sm"],
-      medium: ["rounded-md"],
-      large: ["rounded-lg"],
-      full: ["rounded-full"],
-      none: ["rounded-none"],
-    },
-  },
-  compoundVariants: [
-    { intent: "orange", size: "medium", rounded: "medium", class: "uppercase" },
-  ],
-  defaultVariants: {
-    intent: "orange",
-    size: "medium",
-    rounded: "medium",
-  },
-});
+  }
+);
 
 export interface ButtonProps
   extends HTMLAttributes<HTMLButtonElement>,
